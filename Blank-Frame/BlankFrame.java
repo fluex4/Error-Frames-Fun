628e013931d17a3cc9525433dc4de8c954c04a6a
fluex4 <fluex04@gmail.com>
2023-12-12 21:05:53 +0530

    initial build

--- Blank-Frame/BlankFrame.java
+++ Blank-Frame/BlankFrame.java
@@ -1,1 +1,26 @@
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+public class BlankFrame {
+    public static void main(String[] args) throws InterruptedException {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight(); 
+        JFrame f = new JFrame();
+        f.setUndecorated(true);
+        f.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.1f));
+      f.setSize((int)width,(int)height);
+       f.setAlwaysOnTop(true);
+       f.toFront();
+        f.setVisible(true);
+        //f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+       Thread.sleep(10000);
+
+
+    
+    }
+}
